569a66c2867e6f2ba8b9e9135ac34b2978efef91
mahmoudelhamrawi <mahmoudelhamrawi5@gmail.com>
2023-11-04 11:42:46 +0200

    adding change

--- src/test/java/Helper/Helper.java
+++ src/test/java/Helper/Helper.java
@@ -1,4 +1,29 @@
 package Helper;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Helper {
+
+    public static void capturescreenshot(WebDriver driver, String screenshotname) {
+        Path dest = Paths.get("./screenShots", screenshotname + ".png");
+
+        try {
+            Files.createDirectories(dest.getParent());
+            FileOutputStream out = new FileOutputStream(dest.toString());
+            out.write(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES));
+            out.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 }
--- src/test/java/TestCases/TestBase.java
+++ src/test/java/TestCases/TestBase.java
@@ -3,25 +3,38 @@ package TestCases;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+
+import  Helper.Helper;
 
 public class TestBase {
 
-    protected WebDriver driver ;
+    protected WebDriver driver;
 
     @BeforeClass
-    public void openBrowser(){
-        driver = new ChromeDriver();
+    @Parameters({"browser"})
+    public void openBrowser(@Optional("chrome") String browserName) {
+        if (browserName.equalsIgnoreCase("chrome"))
+        {
+            driver = new ChromeDriver();
+        } else if (browserName.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+        }else if (browserName.equalsIgnoreCase("edge"))
+        {
+            driver = new EdgeDriver();
+        }
+
+
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10,TimeUnit.MILLISECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.MILLISECONDS);
         driver.get("https://demo.nopcommerce.com/");
 
     }
-
-
 
 
     @AfterClass
@@ -30,5 +43,16 @@ public class TestBase {
         driver.quit();
     }
 
+
+    @AfterMethod
+    public void screenShotFailure(ITestResult result) throws IOException {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            System.out.println("fail...");
+            System.out.println("taking screenshot.....");
+            Helper.capturescreenshot(driver, result.getName());
+        }
+
+
+    }
 
 }
